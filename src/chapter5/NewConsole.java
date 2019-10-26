a304fd2db5eed2c9839006b7925f1bba0bbbceff
ayaph <ayaph@champ.aero>
2019-10-27 00:05:46 +0800

    NewConsole

---
+++ src/chapter5/NewConsole.java
@@ -0,0 +1,34 @@
+package chapter5;
+
+import java.io.Console;
+public class NewConsole 
+{
+	public static void main(String[] args)
+	{
+		String name = "";
+		Console c = System.console();
+		char[] pw = null;
+		pw = c.readPassword("%s", "Password: ");
+		for (char ch : pw)
+		{
+			c.format("%c",ch);
+		}
+		c.format("\n");
+		
+		MyUtility mu = new MyUtility();
+		while (true)
+		{
+			name = c.readLine();
+			c.format("Output: %s\n", mu.doStuff(name));
+		}
+		
+	}
+}
+
+class MyUtility
+{
+	String doStuff(String arg1)
+	{
+		return "Result: " + arg1;
+	}
+}
