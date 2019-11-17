b8f4ad72f8308c98b094434505eeae82ca4ba927
ayaph <ayaph@champ.aero>
2019-11-17 10:20:11 +0800

    Add Files Chapter 5

--- src/chapter5/NewConsole.java
+++ src/chapter5/NewConsole.java
@@ -9,16 +9,17 @@ public class NewConsole
 		Console c = System.console();
 		char[] pw = null;
 		pw = c.readPassword("%s", "Password: ");
+		c.format("Password: ");
 		for (char ch : pw)
 		{
-			c.format("%c",ch);
+			c.format("%c ",ch);
 		}
 		c.format("\n");
 		
 		MyUtility mu = new MyUtility();
 		while (true)
 		{
-			name = c.readLine();
+			name = c.readLine("Input: ");
 			c.format("Output: %s\n", mu.doStuff(name));
 		}
 		
